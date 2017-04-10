daf88464b8d38692160a583a228ed22eb7dfe2c9
Philipp Blum <Philipp.Blum@webtrekk.com>
2017-04-10 11:54:36 +0200

    fix dynamic type matching bug

--- src/main/scala/citrullin/orcwriter/types/OrcArray.scala
+++ src/main/scala/citrullin/orcwriter/types/OrcArray.scala
@@ -4,25 +4,8 @@ package citrullin.orcwriter.types
   * Created by citrullin on 25.03.17.
   */
 
-case class OrcListContainer(value: List[OrcType])
-case class OrcSeqContainer(value: Seq[OrcType])
-case class OrcArrayContainer(value: Array[OrcType])
-case class OrcIterableContainer(value: Iterable[OrcType])
-
-class OrcArray(val list: AnyRef) extends OrcType{
-  def isValid: Boolean = list match{
-    case OrcListContainer(_)| OrcArrayContainer(_) |
-         OrcSeqContainer(_) | OrcIterableContainer(_) =>
-      true
-    case _ => false
-  }
-
-  def getValue: List[OrcType] = list match{
-    case OrcListContainer(value) => value
-    case OrcSeqContainer(value) => value.toList
-    case OrcArrayContainer(value) => value.toList
-    case OrcIterableContainer(value) => value.toList
-  }
-
+class OrcArray(val list: List[OrcType]) extends OrcType{
+  def isValid: Boolean = true
+  def getValue: List[OrcType] = list
   def getUncleanValue: Any = list
 }
--- src/test/scala/OrcWriterCreatorSpec.scala
+++ src/test/scala/OrcWriterCreatorSpec.scala
@@ -2,6 +2,8 @@
   * Created by citrullin on 09.04.17.
   */
 
+import java.io.File
+
 import citrullin.orcwriter.types.{OrcMap, _}
 import citrullin.orcwriter.{OrcSchemaCreator, OrcWriter, OrcWriterCreator}
 import org.apache.orc.TypeDescription
@@ -26,11 +28,12 @@ class OrcWriterCreatorSpec extends org.specs2.mutable.Specification{
         )
       )
     })
-
-    val orcWriter: OrcWriter = OrcWriterCreator.createWriter(getCurrentDirectory + "../resources/test.orc", simpleStringStruct)
+    val path = getCurrentDirectory + "/src/test/tmp/test.orc"
+    val orcWriter: OrcWriter = OrcWriterCreator.createWriter(path, simpleStringStruct)
     orcWriter.writeMode = false
 
     orcWriter.write(data)
+    new File(path).delete()
 
     "batch must have" >> {
       "a length of 100" >> {
